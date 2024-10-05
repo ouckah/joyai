563804abf5c161c059f10b0f846db23011475a98
ouckah <aidan.ouckama@gmail.com>
2024-10-05 01:03:04 -0400

    basic chatbot app ui

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -1,19 +1,113 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { useState } from "react";
+import { USER, BOT } from "../../constants/senders";
+import { ScrollContainer } from "../_components/ScrollableContainer";
+import { Navbar } from "../_components/Navbar";
 
 export default function Home() {
+  const [messages, setMessages] = useState([
+    {
+      sender: USER,
+      text: "Hello.",
+    },
+    {
+      sender: BOT,
+      text: "Hello user! How are you?",
+    },
+    {
+      sender: USER,
+      text: "I've been aight.",
+    },
+    {
+      sender: BOT,
+      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+      sender: USER,
+      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+  ]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    if (!inputValue.trim()) return; // avoid empty messages
+
+    const newMessage = {
+      sender: USER,
+      text: inputValue,
+    };
+
+    // update messages array
+    setMessages([...messages, newMessage]);
+
+    // clear input field after sending message
+    setInputValue("");
+  };
+
   return (
-    <div className="flex items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <main className="bg-white flex flex-row gap-8 items-center sm:items-start w-screen h-screen">
-        <div className="flex justify-center items-center w-40 h-screen bg-red-50">
-          <div className="flex justify-center items-center w-full h-96 bg-yellow-100 rounded-full">
-            test
+    <div
+      data-theme="autumn"
+      className="flex items-center justify-items-center w-screen h-screen bg-white pl-12 font-[family-name:var(--font-geist-sans)]"
+    >
+      <main className="bg-white flex flex-row gap-8 items-center sm:items-start w-full h-full">
+        <Navbar />
+        <div className="flex flex-col justify-end items-center w-full h-full gap-14 py-16 bg-white">
+          {/* Messages */}
+          <div className="flex flex-col justify-end items-center w-3/4 h-full overflow-x-hidden">
+            <ScrollContainer>
+              {messages.map((message, i) => {
+                if (message.sender == BOT) {
+                  return (
+                    <div
+                      key={i}
+                      className="chat chat-start self-start w-3/4 text-xl"
+                    >
+                      <div className="chat-image avatar">
+                        <div className="w-20 rounded-full">
+                          <img
+                            alt="JoyAI PFP"
+                            src="https://cdn.discordapp.com/attachments/1013767199493718027/1291960457762050058/images.png?ex=6701ff70&is=6700adf0&hm=841e0d99afc3cd8414f106d04f848656c0d5f3bac2c17e41873f8a81037efd48&"
+                            className="object-cover"
+                          />
+                        </div>
+                      </div>
+                      <div className="flex justify-center items-center chat-header gap-3">
+                        Joy
+                        <time className="text-xs opacity-50">12:45</time>
+                      </div>
+                      <div className="chat-bubble chat-bubble-accent bg-joy-pink">
+                        {message.text}
+                      </div>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div
+                      key={i}
+                      className="chat chat-end self-end w-3/4 text-xl"
+                    >
+                      <div className="chat-bubble chat-bubble-neutral">
+                        {message.text}
+                      </div>
+                    </div>
+                  );
+                }
+              })}
+            </ScrollContainer>
           </div>
-        </div>
-        <div className="flex justify-center items-end w-full h-screen bg-blue-50 p-20">
-          <input
-            className="flex justify-start items-center w-3/4 h-24 bg-green-50 p-10 rounded-full outline-none"
-            placeholder="Ask me anything..."
-          />
+
+          {/* Message input */}
+          <form onSubmit={handleSendMessage} className="w-3/4">
+            <input
+              className="flex justify-start items-center w-full h-24 bg-joy-pink p-10 rounded-full outline-none"
+              placeholder="Ask me anything..."
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+          </form>
         </div>
       </main>
     </div>
---
+++ src/constants/senders.js
@@ -0,0 +1,3 @@
+// constants to store indication of who sent what message
+export const USER = "user";
+export const BOT = "bot";
